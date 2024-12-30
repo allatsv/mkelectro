7ccd3e4787841f125e5c2924a8ae01a8c1725ab4
allatsv <cvetkova20022@gmail.com>
2024-12-30 13:21:53 +0300

    finish

--- carosel-height.js
+++ carosel-height.js
@@ -1,34 +1,34 @@
-const gap = 16;
+const gap56 = 16;
 
-const carousel = document.querySelector(".carousel-height"),
-  content = document.querySelector(".content__product-scroll-imgs"),
-  next = document.querySelector(".btn-bottom"),
-  prev = document.querySelector(".btn-top");
+const carousel5 = document.querySelector(".carousel-height"),
+  content5 = document.querySelector(".content__product-scroll-imgs"),
+  next55 = document.querySelector(".btn-bottom"),
+  prev55 = document.querySelector(".btn-top");
   next5 = document.querySelector(".btn-bottom-grey"),
   prev5 = document.querySelector(".btn-top-grey");
 
-next.addEventListener("click", e => {
-  carousel.scrollBy(width + gap, 0);
-  if (carousel.scrollWidth !== 0) {
-    prev.style.display = "flex";
+next55.addEventListener("click", e => {
+  carousel5.scrollBy(width56 + gap56, 0);
+  if (carousel5.scrollHeight !== 0) {
+    prev55.style.display = "flex";
     prev5.style.display = "none";
   }
-  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-    next.style.display = "none";
+  if (content5.scrollHeight - width56 - gap56 <= carousel5.scrollTop + width56) {
+    next55.style.display = "none";
     next5.style.display = "flex";
   }
 });
-prev.addEventListener("click", e => {
-  carousel.scrollBy(-(width + gap), 0);
-  if (carousel.scrollLeft - width - gap <= 0) {
-    prev.style.display = "none";
+prev55.addEventListener("click", e => {
+  carousel5.scrollBy(-(width56 + gap56), 0);
+  if (carousel5.scrollTop - width56 - gap56 <= 0) {
+    prev55.style.display = "none";
     prev5.style.display = "flex";
   }
-  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
-    next.style.display = "flex";
+  if (!content5.scrollHeight - width56 - gap56 <= carousel5.scrollTop + width56) {
+    next55.style.display = "flex";
     next5.style.display = "none";
   }
 });
 
-let width = carousel.offsetWidth;
-window.addEventListener("resize", e => (width = carousel.offsetWidth));
+let width56 = carousel5.offsetWidth;
+window.addEventListener("resize", e => (width56 = carousel5.offsetWidth));
